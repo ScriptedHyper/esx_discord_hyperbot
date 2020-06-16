c3a05cbc3eecef4fc64e6873d08ab131e52a1d05
ScriptedHyper <54458659+ScriptedHyper@users.noreply.github.com>
2020-06-16 15:27:14 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,133 @@
+-- Modified by Hyper
+
+Config                        = {}
+Config.Locale 				  = 'de'
+Config.green 				  = 56108
+Config.grey 				  = 8421504
+Config.red 					  = 16711680
+Config.orange 				  = 16744192
+Config.blue 				  = 2061822
+Config.purple 				  = 11750815
+Config.webhook                = "YOUR DISCORD WEBHOOH HERE"
+Config.webhook2                = "YOUR DISCORD WEBHOOH HERE"
+Config.webhook3                = "YOUR DISCORD WEBHOOH HERE"
+Config.webhook4                = "YOUR DISCORD WEBHOOH HERE"
+Config.webhook5                = "YOUR DISCORD WEBHOOH HERE"
+Config.webhook6                = "YOUR DISCORD WEBHOOH HERE"
+
+
+
+settings = {
+	LogKills = true, -- Log when a player kill an other player.
+	LogEnterPoliceVehicle = false, -- Log when an player enter in a police vehicle.
+	LogEnterBlackListedVehicle = true, -- Log when a player enter in a blacklisted vehicle.
+	LogPedJacking = false, -- Log when a player is jacking a car
+	LogChatServer = true, -- Log when a player is talking in the chat , /command works too.
+	LogLoginServer = false, -- Log when a player is connecting/disconnecting to the server.
+	LogItemTransfer = true, -- Log when a player is giving an item.
+	LogWeaponTransfer = true, -- Log when a player is giving a weapon.
+	LogMoneyTransfer = true, -- Log when a player is giving money
+	LogMoneyBankTransfert = true, -- Log when a player is giving money from bankaccount
+}
+
+blacklistedModels = {
+	"RHINO",
+	"AKULA",
+	"SAVAGE",
+	"HUNTER",
+	"ANNIHILATOR",
+	"Lazer",
+	"oppressor",
+	"mogul",
+	"volatol",
+	"chernobog",
+	"avenger",
+	"stromberg",
+	"nightshark",
+	"besra",
+	"babushka ",
+	"starling",
+	"cargobob2",
+	"cargobob3",
+	"cargobob4",
+	"caracara",
+	"deluxo",
+	"scramjet",
+	"revolter",
+	"viseris",
+	"savestra",
+	"thruster",
+	"ardent",
+	"dune3",
+	"tampa3",
+	"nokota",
+	"bombushka",
+	"molotok",
+	"pyro",
+	"ruiner2",
+	"limo2",
+	"technical",
+	"technical2",
+	"technical3",
+	"jb700w",
+	"blazer5",
+	"insurgent3",
+	"insurgent",
+	"boxville5",
+	"bruiser",
+	"bruiser2",
+	"bruiser3",
+	"brutus",
+	"brutus2",
+	"brutus3",
+	"cerberus",
+	"cerberus2",
+	"cerberus3",
+	"dominator4",
+	"dominator5",
+	"dominator6",
+	"impaler2",
+	"impaler3",
+	"impaler4",
+	"imperator",
+	"imperator2",
+	"imperator3",
+	"issi4",
+	"issi5",
+	"issi6",
+	"monster3",
+	"monster4",
+	"monster5",
+	"scarab",
+	"scarab2",
+	"scarab3",
+	"slamvan4",
+	"slamvan5",
+	"slamvan6",
+	"zr380",
+	"zr3802",
+	"zr3803",
+	"alphaz1",
+	"avenger2",
+	"blimp",
+	"blimp2",
+	"blimp3",
+	"cuban800",
+	"dodo",
+	"duster",
+	"howard",
+	"jet",
+	"mammatus",
+	"microlight",
+	"miljet",
+	"nimbus",
+	"rogue",
+	"seabreeze",
+	"shamal",
+	"stunt",
+	"titan",
+	"tula",
+	"velum",
+	"velum2",
+	"vestra"
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'modified Discord Bot from Hyper'
+version '1.0.0'
+
+
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/de.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/de.lua',
+	'locales/fr.lua',
+	'client/main.lua'
+}
